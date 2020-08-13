a3f67e5ed458135ceaac6ddb70c84950fd4397ac
Maya-patil <maya.patil@cecsoft.com>
2020-08-13 17:43:26 +0530

    Add design footer

--- src/app/company/company.module.ts
+++ src/app/company/company.module.ts
@@ -5,7 +5,6 @@ import { RouterModule } from '@angular/router';
 import { CompanyComponent } from './company.component';
 import { AddEditUserComponent } from './add-edit-user/add-edit-user.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -17,8 +16,7 @@ import { SharedModule } from '../shared/shared.module';
     CompanyRoutingModule,
     RouterModule,
     FormsModule,
-    ReactiveFormsModule,
-    SharedModule
+    ReactiveFormsModule
   ],
   exports:[
     
