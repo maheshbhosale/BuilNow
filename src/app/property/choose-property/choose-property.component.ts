bfc36cfee37de5e735cf4d93ce9b71d622164a6b
priticecsoft <priti.shreya@cecsoft.com>
2020-08-19 18:09:55 +0530

    Add build page

--- src/app/property/choose-property/choose-property.component.ts
+++ src/app/property/choose-property/choose-property.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as property from '../Json/project.json';
 
 @Component({
   selector: 'app-choose-property',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./choose-property.component.css']
 })
 export class ChoosePropertyComponent implements OnInit {
+  property: any;
+  selectedIndustry = "Residential";
 
   constructor() { }
 
   ngOnInit() {
+    const properties = (property as any).default;
+    console.log(properties);
+    this.property = properties.filter(x => x.ProductLine === this.selectedIndustry);
+    console.log(this.property);
   }
 
 }
