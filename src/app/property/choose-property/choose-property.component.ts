07ee4d494c0cf979f6d1534bbffac8b37b709e6f
Mahesh Bhosale <maheshbhosale939@gmail.com>
2020-08-19 19:28:51 +0530

    Add Feature

--- src/app/property/choose-property/choose-property.component.ts
+++ src/app/property/choose-property/choose-property.component.ts
@@ -10,6 +10,11 @@ export class ChoosePropertyComponent implements OnInit {
   property: any;
   selectedIndustry = "Residential";
 
+  icon = true;
+  icon2 = true;
+  icon3 = true;
+
+
   constructor() { }
 
   ngOnInit() {
@@ -19,4 +24,24 @@ export class ChoosePropertyComponent implements OnInit {
     console.log(this.property);
   }
 
+
+  ToggelIcon(no:number){
+
+    if(no == 1){
+      this.icon = !this.icon;
+      this.icon2 = true;
+      this.icon3  = true;
+
+    }else if(no == 2){
+      this.icon2 = !this.icon2;
+      this.icon = true;
+      this.icon3  = true;
+
+    }else{
+      this.icon3 = !this.icon3;
+      this.icon2 = true;
+      this.icon  = true;
+    }
+
+  }
 }
